13ca51f074234ae8ac89ada3551dd9e3b4081989
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-08 07:48:24 -0400

    model for temp pokemon and controller to fetch data and init project

---
+++ Dex3/views/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Dex3
+//
+//  Created by Jimmy Ghelani on 2023-09-06.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
+        animation: .default)
+    private var pokedex: FetchedResults<Pokemon>
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(pokedex) { pokemon in
+                    NavigationLink {
+                        Text("\(pokemon.id): \(pokemon.name!.capitalized)")
+                    } label: {
+                        Text("\(pokemon.id): \(pokemon.name!.capitalized)")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}

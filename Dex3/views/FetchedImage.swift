824da9c11fd75340dc58e13468231e6ca60cbd54
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-09 09:56:15 -0400

    widget views

---
+++ Dex3/views/FetchedImage.swift
@@ -0,0 +1,26 @@
+//
+//  FetchedImage.swift
+//  Dex3
+//
+//  Created by Jimmy Ghelani on 2023-09-09.
+//
+
+import SwiftUI
+
+struct FetchedImage: View {
+    let url: URL?
+    var body: some View {
+        if let url, let imageData = try? Data(contentsOf: url), let uiImage = UIImage(data: imageData) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: .black, radius: 6)
+        } else {
+            Image(.bulbasaur)
+        }
+    }
+}
+
+#Preview {
+    FetchedImage(url: SamplePokemon.samplePokemon.sprite)
+}
